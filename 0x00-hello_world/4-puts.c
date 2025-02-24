05a084e1c060e7a4507edcfcf3ccaa17124622ef
bkk10 <brkkiprop@gmail.com>
2025-02-24 00:00:35 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a sentence using the puts function.
+ *
+ * Return: 0 if the program executes successfully.
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
+
