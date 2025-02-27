55ff7fe978e70b3b506ebf5d297c64cb9a525acc
bkk10 <brkkiprop@gmail.com>
2025-02-27 11:08:18 -0800

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a sentence to the standard output
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
